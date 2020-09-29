de4381c22751eb2813671b7af62685365d1d0e30
jin sun kyoung <jinsk9268@gmail.com>
2020-09-30 01:01:51 +0900

    Add JS Algorithm

---
+++ JavaScript/sortTest_0825.js
@@ -0,0 +1,45 @@
+function solution(array, commands) {
+  let answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let addArr = array.slice(commands[i][0] - 1, commands[i][1]);
+    addArr.sort((a, b) => a - b);
+    answer.push(addArr[commands[i][2] - 1]);
+  }
+  return answer;
+}
+
+console.log(
+  solution(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3]
+    ]
+  )
+);
+
+// arrow function
+const solutionArrow = (array, commands) => {
+  return commands.map(command => {
+    const [startIndex, endIndex, returnIndex] = command;
+    const resultArray = array
+      .filter(
+        (value, index) => index >= startIndex - 1 && index <= endIndex - 1
+      )
+      .sort((a, b) => a - b);
+
+    return resultArray[returnIndex - 1];
+  });
+};
+
+console.log(
+  solutionArrow(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3]
+    ]
+  )
+);
