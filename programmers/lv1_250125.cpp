81379469f2069c0c60ece04d98a744fbc5718008
jinsk9268 <jinsk9268@gmail.com>
2024-10-14 11:01:39 +0900

    [250125] 이웃한 칸

---
+++ programmers/lv1_250125.cpp
@@ -0,0 +1,32 @@
+/*
+문제 : 이웃한 칸
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/250125
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> board, int h, int w)
+{
+    int answer = 0;
+
+    string color = board[h][w];
+    vector<int> direction = {-1, 1};
+    for (int i : direction)
+    {
+
+        if ((h + i >= 0 && h + i < board.size()) && board[h + i][w] == color)
+        {
+            answer += 1;
+        }
+
+        if ((w + i >= 0 && w + i < board[0].size()) && board[h][w + i] == color)
+        {
+            answer += 1;
+        }
+    }
+
+    return answer;
+}
